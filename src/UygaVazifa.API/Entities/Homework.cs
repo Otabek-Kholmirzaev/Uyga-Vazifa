9e2138d94397ac1385db96660bddae9a12733bc8
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2022-12-17 21:54:19 +0500

    aded homework entity

---
+++ src/UygaVazifa.API/Entities/Homework.cs
@@ -0,0 +1,8 @@
+﻿namespace UygaVazifa.API.Entities;
+
+public class Homework
+{
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public string? FilePath { get; set; }
+}
