b31088568ff13c8d62b65b2b3124c91335ec9853
Leverow <info.enjoymedia@mail.ru>
2022-12-17 19:18:14 +0500

    Initialized Identity

--- src/UygaVazifa.API/Data/AppDbContext.cs
+++ src/UygaVazifa.API/Data/AppDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using UygaVazifa.API.Entities;
 
 namespace UygaVazifa.API.Data;
 
-public class AppDbContext : IdentityDbContext
+public class AppDbContext : IdentityDbContext<User, Role, Guid>
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
 }
--- src/UygaVazifa.API/Program.cs
+++ src/UygaVazifa.API/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using UygaVazifa.API.Data;
+using UygaVazifa.API.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,18 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseNpgsql(builder.Configuration.GetConnectionString("PostgresConnection"));
 });
+
+builder.Services.AddIdentity<User, Role>(options =>
+    {
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequiredLength = 6;
+        options.Password.RequireUppercase = false;
+        options.Password.RequireDigit = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequireLowercase = false;
+    })
+    .AddEntityFrameworkStores<AppDbContext>();
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
