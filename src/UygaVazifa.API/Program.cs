3fb1641faee16125066dacc4f30ebd1ce739a13e
Leverow <info.enjoymedia@mail.ru>
2022-12-18 11:10:57 +0500

    Repository pattern fixed

--- src/UygaVazifa.API/Program.cs
+++ src/UygaVazifa.API/Program.cs
@@ -2,6 +2,8 @@ using FurnitureShop.Common.Helpers;
 using Microsoft.EntityFrameworkCore;
 using UygaVazifa.API.Data;
 using UygaVazifa.API.Entities;
+using UygaVazifa.API.Repositories;
+using UygaVazifa.API.Repositories.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +15,12 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseNpgsql(builder.Configuration.GetConnectionString("PostgresConnection"));
 });
+
+builder.Services.AddScoped<IGenericRepository<Homework>, GenericRepository<Homework>>();
+builder.Services.AddScoped<IGenericRepository<Comment>, GenericRepository<Comment>>();
+builder.Services.AddScoped<IGenericRepository<Group>, GenericRepository<Group>>();
+builder.Services.AddScoped<IGenericRepository<StudentAnswer>, GenericRepository<StudentAnswer>>();
+builder.Services.AddScoped<IGenericRepository<UserGroup>, GenericRepository<UserGroup>>();
 
 builder.Services.AddIdentity<User, Role>(options =>
     {
