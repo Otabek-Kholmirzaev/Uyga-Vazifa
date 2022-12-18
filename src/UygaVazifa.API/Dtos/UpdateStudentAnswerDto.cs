4c0e4091d8f28a152efe944b44e3b48073c253b4
Leverow <info.enjoymedia@mail.ru>
2022-12-18 10:29:12 +0500

    Update entity dto added

---
+++ src/UygaVazifa.API/Dtos/UpdateStudentAnswerDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using UygaVazifa.API.Entities;
+using UygaVazifa.API.Entities.Enums;
+
+namespace UygaVazifa.API.Dtos;
+
+public class UpdateStudentAnswerDto
+{
+    public string? File { get; set; }
+    public ECommentType CommentType { get; set; }
+
+    public Guid? HomeworkId { get; set; }
+    public Guid StudentId { get; set; }
+
+    public EResultStudentAnswer? Result { get; set; }
+    public EStudentAnswerStatus? Status { get; set; }
+}
