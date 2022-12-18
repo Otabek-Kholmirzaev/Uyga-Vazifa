8cbc21cde1cbdc6a933769680b9ac6fa123d1b5d
Leverow <info.enjoymedia@mail.ru>
2022-12-18 10:18:10 +0500

    Added interface of group service

---
+++ src/UygaVazifa.API/Services/IGroupService.cs
@@ -0,0 +1,14 @@
+﻿using FurnitureShop.Common.Models;
+using UygaVazifa.API.Dtos;
+using UygaVazifa.API.Models;
+
+namespace UygaVazifa.API.Services;
+
+public interface IGroupService
+{
+    Task<Result<List<GroupResponseModel>>> GetAllAsync(PaginationParams paginationParams);
+    Task<Result<GroupResponseModel>> GetByIdAsync(Guid groupId);
+    Task<Result> AddAsync(CreateGroupDto dtoModel);
+    Task<Result> RemoveAsync(Guid groupId);
+    Task<Result<GroupResponseModel>> UpdateAsync(Guid groupId, UpdateGroupDto dtoModel);
+}
