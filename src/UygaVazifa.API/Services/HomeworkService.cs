e9568b52cd67daa7cbe9e31e0ceccfc54fb80e9d
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2022-12-18 12:36:10 +0500

    added implimentation of IHomeworksService

--- src/UygaVazifa.API/Services/HomeworkService.cs
+++ src/UygaVazifa.API/Services/HomeworkService.cs
@@ -1,6 +1,42 @@
-﻿namespace UygaVazifa.API.Services;
+﻿using FurnitureShop.Common.Models;
+using UygaVazifa.API.Dtos;
+using UygaVazifa.API.Entities;
+using UygaVazifa.API.Models;
+using UygaVazifa.API.Repositories.Interfaces;
+
+namespace UygaVazifa.API.Services;
 
 public class HomeworkService : IHomeworksService
 {
+    private readonly IGenericRepository<Homework> _homeworkRepository;
 
+    public HomeworkService(IGenericRepository<Homework> homeworkRepository)
+    {
+        _homeworkRepository = homeworkRepository;
+    }
+
+    public async Task<Result<List<HomeworkResponseModel>>> GetAllAsync(PaginationParams paginationParams)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Result<HomeworkResponseModel>> GetByIdAsync(Guid homeworkId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Result> AddAsync(CreateHomeworkDto dtoModel)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Result> RemoveAsync(Guid groupId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Result<HomeworkResponseModel>> UpdateAsync(Guid homeworkId, UpdateGroupDto dtoModel)
+    {
+        throw new NotImplementedException();
+    }
 }
--- src/UygaVazifa.API/Services/IHomeworksService.cs
+++ src/UygaVazifa.API/Services/IHomeworksService.cs
@@ -1,6 +1,14 @@
-﻿namespace UygaVazifa.API.Services;
+﻿using FurnitureShop.Common.Models;
+using UygaVazifa.API.Dtos;
+using UygaVazifa.API.Models;
+
+namespace UygaVazifa.API.Services;
 
 public interface IHomeworksService
 {
-
+    Task<Result<List<HomeworkResponseModel>>> GetAllAsync(PaginationParams paginationParams);
+    Task<Result<HomeworkResponseModel>> GetByIdAsync(Guid homeworkId);
+    Task<Result> AddAsync(CreateHomeworkDto dtoModel);
+    Task<Result> RemoveAsync(Guid groupId);
+    Task<Result<HomeworkResponseModel>> UpdateAsync(Guid homeworkId, UpdateGroupDto dtoModel);
 }
