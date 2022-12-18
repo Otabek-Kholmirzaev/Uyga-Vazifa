c39f826b58707ab0ecb4a3775f56ca4ec2c0c1c7
Leverow <info.enjoymedia@mail.ru>
2022-12-18 10:05:26 +0500

    Create homework dto added

---
+++ src/UygaVazifa.API/Dtos/CreateHomeworkDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UygaVazifa.API.Dtos;
+public class CreateHomeworkDto
+{
+    [Required]
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public List<IFormFile>? Files { get; set; }
+    [Required]
+    public DateTime? StartDate { get; set; }
+    [Required]
+    public DateTime? EndDate { get; set; }
+
+    [Required]
+    public Guid GroupId { get; set; }
+}
