1ccf25df5287d8aca572a839c38e77b2c1210730
Otabek Kholmirzaev <otabekmirzo@outlook.com>
2022-12-18 13:16:48 +0500

    Added GroupsController

---
+++ src/UygaVazifa.API/Controllers/GroupsController.cs
@@ -0,0 +1,89 @@
+﻿using FurnitureShop.Common.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using UygaVazifa.API.Dtos;
+using UygaVazifa.API.Entities;
+using UygaVazifa.API.Models;
+using UygaVazifa.API.Services;
+
+namespace UygaVazifa.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public partial class GroupsController : ControllerBase
+{
+    private readonly UserManager<User> _userManager;
+
+    private readonly IGroupService _groupService;
+
+    private readonly IHomeworksService _homeworkService;
+
+    public GroupsController(IGroupService groupService, IHomeworksService homeworkService)
+    {
+        _groupService = groupService;
+        _homeworkService = homeworkService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllGroups([FromQuery] PaginationParams paginationParams) => Ok(await _groupService.GetAllAsync(paginationParams));
+
+    [HttpGet("{groupId:guid}")]
+    public async Task<IActionResult> GetGroupById(Guid groupId)
+    {
+        var existingGroupResult = await _groupService.GetByIdAsync(groupId);
+        
+        return HandleResultResponseModel(existingGroupResult);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateGroup([FromBody] CreateGroupDto dtoModel)
+    {
+        if(!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        // faqat admin group create qila oladi
+        var user = await _userManager.GetUserAsync(User);
+        var userGroup = new UserGroup() { UserId = user.Id };
+
+        dtoModel.Users?.Add(userGroup);
+
+        var createdGroupResult = await _groupService.AddAsync(dtoModel);
+        
+        return HandleResultResponseModel(createdGroupResult);
+    }
+
+    [HttpPut("groupId:guid")]
+    public async Task<IActionResult> UpdateGroup(Guid groupId, [FromBody] UpdateGroupDto dtoModel)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        var updatedGroupResult = await _groupService.UpdateAsync(groupId, dtoModel);
+
+        return HandleResultResponseModel(updatedGroupResult);
+    }
+
+    [HttpDelete("{groupId:guid}")]
+    public async Task<IActionResult> DeleteGroup(Guid groupId)
+    {
+        var deletedGroupResult = await _groupService.RemoveAsync(groupId);
+
+        return HandleResultResponseModel(deletedGroupResult);
+    }
+
+    private ObjectResult HandleResultResponseModel(Result existingObjectResult)
+    {
+        if(!existingObjectResult.IsSuccess)
+        {
+            ObjectResult? errorHandler = existingObjectResult.ErrorCode switch
+            {
+                StatusCodes.Status404NotFound => NotFound(existingObjectResult),
+                _ => BadRequest(existingObjectResult)
+            };
+            if (errorHandler is not null)
+                return errorHandler;
+        }
+        return Ok(existingObjectResult);
+    }
+}
--- src/UygaVazifa.API/Dtos/CreateGroupDto.cs
+++ src/UygaVazifa.API/Dtos/CreateGroupDto.cs
@@ -1,13 +1,16 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using UygaVazifa.API.Entities;
 
 namespace UygaVazifa.API.Dtos;
 
 public class CreateGroupDto
 {
     [Required]
-    public string? Name { get; set; }
+    public string Name { get; set; }
 
     [Required]
     public Guid TeacherId { get; set; }
     public Guid AssistantId { get; set; }
+    public List<UserGroup>? Users { get; set; }
+
 }
