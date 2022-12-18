fdcfa347e778b2e0694d9b4b42dc9dfb4e60c338
Leverow <info.enjoymedia@mail.ru>
2022-12-18 08:27:11 +0500

    Added create group dto

---
+++ src/UygaVazifa.API/Dtos/CreateGroupDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UygaVazifa.API.Dtos;
+
+public class CreateGroupDto
+{
+    [Required]
+    public string? Name { get; set; }
+
+    [Required]
+    public Guid TeacherId { get; set; }
+    public Guid AssistantId { get; set; }
+}
